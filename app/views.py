ac84b63d554fa2ffc252fc50b569420401852106
Max Rabota <maxadrap@gmail.com>
2023-03-18 17:43:48 +0200

    1

--- app/views.py
+++ app/views.py
@@ -1,7 +1,5 @@
-from rest_framework.generics import CreateAPIView
-from rest_framework.generics import DestroyAPIView
-from rest_framework.generics import ListAPIView
-from rest_framework.generics import UpdateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from app.models import ToDo
 from app.serializers import ToDoSerializer
@@ -10,21 +8,44 @@ from app.serializers import ToDoSerializer
 #CRUD operation
 
 
-class ListToDo(ListAPIView):
-    queryset = ToDo.objects.all()
-    serializer_class = ToDoSerializer
+class ListToDo(APIView):
+    def get(self, request):
+        queryset = ToDo.objects.all()
+        serializer = ToDoSerializer(queryset, many=True)
+        return Response(serializer.data)
 
-class UpdateToDo(UpdateAPIView):
-    queryset = ToDo.objects.all()
-    serializer_class = ToDoSerializer
+    def post(self, request):
+        serializer = ToDoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
 
-class CreateTodo(CreateAPIView):
-    queryset = ToDo.objects.all()
-    serializer_class = ToDoSerializer
 
-class DeleteToDo(DestroyAPIView):   # delete
-    queryset = ToDo.objects.all()
-    serializer_class = ToDoSerializer
+
+
+
+
+
+
+
+
+
+
+
+
+# class UpdateToDo(UpdateAPIView):
+   #  queryset = ToDo.objects.all()
+    # serializer_class = ToDoSerializer
+
+# class CreateTodo(CreateAPIView):
+    # queryset = ToDo.objects.all()
+    # serializer_class = ToDoSerializer
+
+# class DeleteToDo(DestroyAPIView):   # delete
+   #  queryset = ToDo.objects.all()
+    # serializer_class = ToDoSerializer
 
 
 
